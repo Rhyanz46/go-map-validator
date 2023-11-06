05282256abadff4bfc7481fb663160187af19b77
Rhyanz46 <rianariansaputra@gmail.com>
2023-11-07 05:24:22 +0800

    update vars

--- map_validator/vars.go
+++ map_validator/vars.go
@@ -2,6 +2,8 @@ package map_validator
 
 import "errors"
 
-var ErrNoData = errors.New("no validatorType")
-var ErrInvalidFormat = errors.New("validatorType format invalid")
-var ErrUnsupportType = errors.New("type is not support")
+var (
+	ErrNoData        = errors.New("no validatorType")
+	ErrInvalidFormat = errors.New("validatorType format invalid")
+	ErrUnsupportType = errors.New("type is not support")
+)
